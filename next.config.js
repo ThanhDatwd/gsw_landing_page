613ff524ce797b4954ecd0950bde4d7b7e056935
nguyenthanhdat <87852737+ThanhDatwd@users.noreply.github.com>
2024-01-31 17:16:34 +0700

    third  commit

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "dvsk-fawn.vercel.app"],
+    domains: ["localhost", "greensoftware-seven.vercel.app"],
   },
 };
 
